69f8634a6d1f14760915262ef701b290873d92b7
Mohd Shariq <mohdshariq8589@gmail.com>
2024-11-05 22:29:54 +0530

    create a middleware

--- Mern-Real/api/controllers/auth.controller.js
+++ Mern-Real/api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 
-export const signup = (req, res) => {
+
+export const signup = (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({username ,email ,password: hashedPassword });
@@ -10,12 +11,8 @@ export const signup = (req, res) => {
         newUser.save();
         res.status(201).json(" User Created successfully ");        
     } catch (error) {
-        res.status(500).json(error.message);
+        next(error);
     }
-
-
     // async await use karna hai
-    newUser.save();
-    res.status(201).json(" User Created successfully ");
 
 };
