5b119a9b4b1563c70180f158782563dbb985049b
Mohd Shariq <mohdshariq8589@gmail.com>
2024-11-12 00:51:52 +0530

    with

--- Mern-Real/client/src/components/Private_Route.jsx
+++ Mern-Real/client/src/components/Private_Route.jsx
@@ -1,7 +1,8 @@
 import { useSelector } from "react-redux";
-import { Outlet , Navigate  } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 
 export default function Private_Route() {
-    const {currentUser} = useSelector((state) => state.user)
-  return currentUser ? <Outlet /> : <h1> Sign in to view this page</h1>
+  const { currentUser } = useSelector((state) => state.user);
+  return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
 }
+ 
